c05f2f4d8450f99dd9600757cf7db4e603c6f23c
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-07-22 18:50:46 +0200

    add routes for employees

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// Company
+use App\Http\Controllers\Api\Company\PostCreateCompanyController;
+use App\Http\Controllers\Api\Company\PostActivateCompanyController;
+use App\Http\Controllers\Api\Company\PostListCompanyController;
+
+// Employee
+use App\Http\Controllers\Api\Employee\PostCreateEmployeeController;
+use App\Http\Controllers\Api\Employee\PostActivateEmployeeController;
+use App\Http\Controllers\Api\Employee\PostListEmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +23,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+| Company Routes
+|--------------------------------------------------------------------------
+*/
+
 // Create new company
-Route::post('/company', [App\Http\Controllers\Api\Company\PostCreateCompanyController::class, '__invoke']);
+Route::post('/company', [PostCreateCompanyController::class, '__invoke']);
 
 // Activate a company
-Route::post('/company/activate', [App\Http\Controllers\Api\Company\PostActivateCompanyController::class, '__invoke']);
+Route::post('/company/activate', [PostActivateCompanyController::class, '__invoke']);
 
 // List companies
-Route::get('/company', [App\Http\Controllers\Api\Company\PostListCompanyController::class, '__invoke']);
+Route::get('/company', [PostListCompanyController::class, '__invoke']);
+
+
+/*
+|--------------------------------------------------------------------------
+| Employee Routes
+|--------------------------------------------------------------------------
+*/
+
+// Create new employee
+Route::post('/employee', [PostCreateEmployeeController::class, '__invoke']);
+
+// Activate a employee
+Route::post('/employee/activate', [PostActivateEmployeeController::class, '__invoke']);
+
+// List employees
+Route::get('/employee', [PostListEmployeeController::class, '__invoke']);
